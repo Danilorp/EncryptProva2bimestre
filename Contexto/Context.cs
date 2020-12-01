60d3ceb991ebd687ff99ec8d5aff22984ec8c8a4
Danilo Pinha <danilorpinha@hotmail.com>
2020-11-30 21:28:32 -0300

    Add project files.

---
+++ Contexto/Context.cs
@@ -0,0 +1,16 @@
+﻿using EncryptProva.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EncryptProva.Contexto
+{
+    public class Context : DbContext
+    {
+        public DbSet<dadosencrypt> dadosencrypts { get; set; }
+
+
+    }
+}
---
+++ Models/dadosencrypt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EncryptProva.Models
+{
+    public class dadosencrypt
+    {
+        [Key]
+        public int id { get; set; }
+        [Required(ErrorMessage = "Obrigatorio preenchimento")]
+        [Display(Name = "Texto: ")]
+        public string campo { get; set; }
+        
+
+    
+    }
+}
